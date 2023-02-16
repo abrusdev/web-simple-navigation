da18fa36d02824acd0dc41fedf5ff65f06894ea9
abrusdev <abrusdev@gmail.com>
2023-02-16 15:53:43 +0500

    add: Accordion.js and AccordionPage.js

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,9 +1,47 @@
-function Accordion() {
-  return (
-    <div>
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 
+function Accordion({ items }) {
+
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setExpandedIndex((currentExpandedIndex) => {
+      return index === currentExpandedIndex ? -1 : index;
+    })
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
+
+    return (
+      <div key={item.id} style={{ width: 500 }}>
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+             onClick={() => handleClick(index)}>
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
+      </div>
+    );
+  });
+
+  return (
+    <div className="flex justify-center m-5">
+      <div className="border-x border-t rounded">
+        {renderedItems}
+      </div>
     </div>
   )
+}
+
+Accordion.propTypes = {
+  items: PropTypes.array
 }
 
 export default Accordion;
--- src/pages/AccordionPage.js
+++ src/pages/AccordionPage.js
@@ -1,7 +1,24 @@
+import Accordion from "../components/accordion";
+
 function AccordionPage() {
+  const items = [
+    {
+      id: 1,
+      label: 'What is React JS?',
+      content: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly ' +
+        'used to demonstrate the visual form of a document or a typeface without relying on meaningful content. '
+    },
+    {
+      id: 2,
+      label: 'What is Next JS?',
+      content: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly ' +
+        'used to demonstrate the visual form of a document or a typeface without relying on meaningful content. '
+    }
+  ]
+
   return (
     <div>
-
+      <Accordion items={items} />
     </div>
   )
 };
